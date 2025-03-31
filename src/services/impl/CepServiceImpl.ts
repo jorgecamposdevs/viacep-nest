e4e49ddd227ed8a5d062cb607f3e7b6b0a69daac
Jorge Campos de Jesus <jorgecamposdevs@gmail.com>
2025-03-31 17:57:00 -0300

    implemented logger

--- src/services/impl/CepServiceImpl.ts
+++ src/services/impl/CepServiceImpl.ts
@@ -1,12 +1,14 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { CepService } from '../CepService';
 import { CepResponseDto } from '../../dtos/responses/CepResponseDTO';
 import axios from 'axios';
 
 @Injectable()
 export class CepServiceImpl implements CepService {
+  private readonly logger = new Logger(CepServiceImpl.name);
   async getCep(cep: string): Promise<CepResponseDto> {
     const url = `https://viacep.com.br/ws/${cep}/json/`;
+    this.logger.log(`Iniciando busca do CEP: ${cep} - URL: ${url}`);
     const { data } = await axios.get<CepResponseDto>(url);
     return data;
   }
